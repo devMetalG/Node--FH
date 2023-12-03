a519deffc58dba9b7326f61bb4ea803cf89caea7
Guillermo Franco <fra_borjon@hotmail.com>
2023-12-02 19:51:44 -0600

    add: FileSystem

--- 01-fundamentos/app2.js
+++ 01-fundamentos/app2.js
@@ -3,6 +3,6 @@ const fs = require('fs')
 const data = fs.readFileSync('README.md', 'utf8')
 
 const newData = data.replace(/React/ig, '##Angular')
-fs.writeFileSync('README-Angular.mc', newData)
+fs.writeFileSync('README-Angular.md', newData)
 
 console.log(newData)
